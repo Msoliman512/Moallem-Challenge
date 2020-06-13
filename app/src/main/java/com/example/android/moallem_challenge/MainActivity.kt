053b4d48707eb2b393d63ba5a47761304cba4e41
Msoliman512 <mesaam17@gmail.com>
2020-06-13 18:04:14 +0200

    Add custom icon to the overflow menu in the actionbar

--- app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
+++ app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.android.moallem_challenge
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +21,11 @@ class MainActivity : AppCompatActivity() {
       //  val root = LayoutInflater.from(applicationContext).inflate(R.layout.activity_main, null)
         supportActionBar?.elevation = 0F
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overflow_menu, menu)
+        return true
     }
 }
