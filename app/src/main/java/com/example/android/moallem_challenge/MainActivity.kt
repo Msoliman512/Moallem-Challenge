c25eb27e8014deff61bd2d7ba7c82b098213fd07
Msoliman512 <mesaam17@gmail.com>
2020-06-13 09:20:35 +0200

    Change the activity color to white (Blind the action bar with the activity)

--- app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
+++ app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.android.moallem_challenge
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.setBackgroundColor(Color.WHITE);
+        val root = LayoutInflater.from(applicationContext).inflate(R.layout.activity_main, null)
         supportActionBar?.elevation = 0F
 
     }
