47187b767a6b2cfb49860b0e9fa59acc9cbd2634
Msoliman512 <mesaam17@gmail.com>
2020-06-13 14:56:05 +0200

    Add Overflow Menu to res

--- app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
+++ app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.setBackgroundColor(Color.WHITE);
+        window.decorView.setBackgroundColor(Color.WHITE)
       //  val root = LayoutInflater.from(applicationContext).inflate(R.layout.activity_main, null)
         supportActionBar?.elevation = 0F
 
