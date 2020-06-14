1b35139c17b1027ef09b834c16c950c830d4dd58
Msoliman512 <mesaam17@gmail.com>
2020-06-14 13:21:33 +0200

    Add SubjectsAdapter.kt

--- app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
+++ app/src/main/java/com/example/android/moallem_challenge/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.android.moallem_challenge
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,14 +12,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
-
-
+    var subjects: ArrayList<Subject> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.setBackgroundColor(Color.WHITE)
         supportActionBar?.elevation = 0F
-
+        subjects.add(Subject("Physics", R.drawable.physics))
+        subjects.add(Subject("Biology", R.drawable.biology))
+        subjects.add(Subject("History", R.drawable.history))
+        subjects.add(Subject("Algebra", R.drawable.algebra))
+        subjects.add(Subject("Literature", R.drawable.literature))
+        recyclerView.layoutManager = LinearLayoutManager(this)
+         recyclerView.adapter = SubjectsAdapter(subjects)
 
     }
 
---
+++ app/src/main/java/com/example/android/moallem_challenge/SubjectsAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.android.moallem_challenge
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SubjectsAdapter(val subjects: ArrayList<Subject>) : RecyclerView.Adapter<SubjectsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubjectsAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SubjectsAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
